ae31a07692d02a37f327e9feb46dec02063e9c1a
Tarulia <mihawk90@users.noreply.github.com>
2021-04-18 15:34:36 +0200

    Update script for automated build/install of RPM
    * Always updates the key, as chances are the key has run out by the time there is an update.
    * Optional argument `n`/`nobuild`/`--nobuild` to suppress building and installing of the package as to only update the key.
    * Optional `frel` override for Fedora Release version. Useful for testing future release builds.
    * RPM installation can be skipped by simply cancelling the `sudo` prompt.
    
    Note: This does *not* (yet?) do the required steps of installing RPM Fusion and `mock-rpmfusion`, since these are one-time operations. This *can* be easily added, but I'm not sure it's worth the bloat.
    
    Could also make the build optional by providing an argument (`b`/`build`/`--build`), i.e. only update key when no argument is provided. I'm not sure which use-case happens more often (since this is my first time installing/using MakeMKV).
    
    Thoughts?
    
    Also updated the README accordingly.

---
+++ update.sh
@@ -0,0 +1,42 @@
+#!/bin/sh
+
+if   [ "$1" != "nobuild" ] \
+  && [ "$1" != "n" ] \
+  && [ "$1" != "--nobuild" ]; then
+
+	git pull --ff-only
+	
+	spectool -g makemkv.spec
+	
+	if [ "$1" == "frel" ] && [ "$2" != "" ]; then
+		frel=$2
+		echo "Fedora Release version specified. Using $frel."
+	else
+		frel=$(rpm -E %fedora)
+		echo "No Fedora Release version specified. Using default $frel."
+	fi;
+
+	# delimited by spaces, every space is a new "field" for cut, hence field 9 for the version/release
+	mver=$(cat makemkv.spec | grep Version\: | cut -d" " -f9)
+	rver=$(cat makemkv.spec | grep Release\: | cut -d" " -f9 | sed -e 's/%{?dist}//')
+	
+	mock -r fedora-$frel-x86_64-rpmfusion_nonfree --sources=. --spec=makemkv.spec
+	
+	cp /var/lib/mock/fedora-$frel-x86_64/result/makemkv-$mver-$rver.fc$frel.*.rpm .
+	echo "Copied RPM to current directory. Enter sudo-password to install using DNF. Press Ctrl-c to cancel installation."
+	sudo dnf install makemkv-$mver-$rver.fc$frel.x86_64.rpm
+else
+	echo "--nobuild argument provided - updating key only";
+fi;
+
+# extract current beta "license" key from the buy website and put it in the settings file
+if [ ! -f ~/.MakeMKV/settings.conf ]; then
+	echo "CREATE settings.conf using current beta key."
+	mkdir -p ~/.MakeMKV
+	echo "app_Key = \"$(curl https://www.makemkv.com/buy/ | grep \<code\> | sed -e "s/<code>//; s/<\/code>//")\"" > ~/.MakeMKV/settings.conf
+else
+	echo "UPDATE settings.conf using current beta key."
+	sed -i "s/^app_Key.*/app_Key = \"$(curl https://www.makemkv.com/buy/ | grep \<code\> | sed -e "s/<code>//; s/<\/code>//")\"/" ~/.MakeMKV/settings.conf
+fi;
+
+echo "All done!"
